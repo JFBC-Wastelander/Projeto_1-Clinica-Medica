9677e2f58a6ef86a84b4e0b80bc903fc8c7923ee
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-25 19:28:11 -0300

    remake: refeito a estrutura do código

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
-let input
-console.log("Bem vindo a clínica médica")
-console.log('Tecle "1" para criar uma consulta médica.\nTecle "2" para ver suas consultas.\nTecle "3" se quiser atualizar uma consulta já existente.\nTecle "4" se quiser cancelar uma consulta')
-process.stdin.on("data", function(data){
-    input = +data.toString().trim()
-    switch (input) {
-        case 1:
-            console.log('Criar consulta')
-        break
-        case 2:
-            console.log('Ver consulta')
-        break
-        case 3:
-            console.log('Atualizar consulta')
-        break
-        case 4:
-            console.log('Cancelar consulta')
-        break
+const consulta = [];
+let variavel = "menu";
+let input;
+let usuario = {
+  nome: "",
+  idade: "",
+  sintoma: "",
+  dia: "",
+  hora: "",
+};
+
+console.log("Bem-vindo a clínica médica. Selecione uma opção:");
+console.log(
+  "1. Adicionar uma consulta\n2. Ver consultas.\n3. Atualizar uma consulta.\n4. Cancelar uma consulta.\n5. Encerrar"
+);
+
+process.stdin.on("data", function (data) {
+  input = data.toString().trim();
+
+  if (variavel === "menu") {
+    if (input === "1") {
+      variavel = "adicionarConsulta";
+      console.log("Por favor informe seu usuario");
+    } else if (input === "2") {
+      variavel = "verConsulta";
+      console.log(consulta);
+      variavel = "menu";
+    } else if (input === "3") {
+      variavel = "atualizarConsulta";
+      console.log("Qual consulta deseja alterar?");
+    } else if (input === "4") {
+      variavel === "cancelarConsulta";
+      console.log("Qual consulta deseja cancelar?");
+    } else if (input === "5") {
+      console.log("Saindo...");
+      process.exit();
     }
-})
+  } else if (variavel === "adicionarConsulta") {
+    usuario.nome = input;
+    variavel = "adicionarIdade";
+    console.log("Digite sua idade");
+  } else if (variavel === "adicionarIdade") {
+    usuario.idade = input;
+    variavel = "adicionarDoença";
+    console.log("Informe seus sintomas");
+  } else if (variavel === "adicionarDoença") {
+    usuario.sintoma = input;
+    variavel = "adicionarDia";
+    console.log("Qual dia deseja realizar a consulta?");
+  } else if (variavel === "adicionarDia") {
+    usuario.dia = input;
+    variavel = "adicionarHora";
+    console.log("Qual horário deseja para realizar a consulta?");
+  } else if (variavel === "adicionarHora") {
+    usuario.hora = input;
+    variavel = "menu";
+    consulta.push(usuario);
+    usuario = {
+      nome: "",
+      idade: "",
+      sintoma: "",
+      dia: "",
+      hora: "",
+    };
+    console.log("Consulta cadastrada. Selecione uma opção");
+    console.log(
+      "1. Adicionar uma consulta\n2. Ver consultas.\n3. Atualizar uma consulta.\n4. Cancelar uma consulta.\n5. Encerrar"
+    );
+  } else if (variavel === "cancelarConsulta") {
+    console.log("");
+  }
+});
